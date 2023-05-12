8e5ed383039159059d0f5428960d3aa20a3bcccf
TunazZz <121681379+vantuan0128@users.noreply.github.com>
2023-05-13 01:13:21 +0700

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,50 @@
+let allMusic = [
+    {
+        name: "Có Chắc Yêu Là Đây",
+        artist: "Sơn Tùng M-TP",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Making My Way",
+        artist: "Sơn Tùng M-TP",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Nơi này có anh",
+        artist: "Sơn Tùng M-TP",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Ta Không Thuộc Về Nhau",
+        artist: "Sơn Tùng M-TP",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Như Ngày Hôm Qua",
+        artist: "Sơn Tùng M-TP",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Chúng Ta Của Hiện Tại",
+        artist: "Sơn Tùng M-TP",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "Âm Thầm Bên Em",
+        artist: "Sơn Tùng M-TP",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "Lạc Trôi",
+        artist: "Sơn Tùng M-TP",
+        img: "music-8",
+        src: "music-8"
+    },
+];
